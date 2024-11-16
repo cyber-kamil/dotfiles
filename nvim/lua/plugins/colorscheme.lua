c953eeafdecdfad1749e2735162fd5b2324bb963
cyber-kamil <kamil.dev@outlook.com>
2024-11-16 17:24:03 +0000

    added new nvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  "rose-pine/neovim", name = "rose-pine",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+
+    {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
